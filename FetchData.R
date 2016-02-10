14501190dab703b7fb7ee5f96339c903aa51e8e8
Brian Fannin <BFannin@redwoodsgroup.com>
2016-02-10 14:56:46 -0500

    More cleanup

--- FetchData.R
+++ FetchData.R
@@ -1,13 +1,12 @@
 library(dplyr)
 
-download <- TRUE
-
-strDownload <- "CrashData.csv.gz"
+download <- FALSE
 
 if (download){
-  
+
+  strDownload <- "CrashData.csv.gz"  
   bikeSafetyURL <- "https://github.com/BikeSafety/BikeSafety/blob/master/data/North_Carolina_Bicycle_Crash_Data.csv.gz?raw=true"
-  download.file(bikeSafetyURL, strDownload)
+  download.file(bikeSafetyURL, strDownload, mode="wb")
   rawData <- read.csv(gzfile(strDownload), stringsAsFactors = FALSE)
   unlink(strDownload)
   rawData %>% write.csv("RawData.csv", row.names = FALSE)
@@ -19,12 +18,24 @@ if (download){
 }
 
 cleanData <- rawData %>% 
-  rename(CrashMonth = Crash_Mont
+  rename(Ambulance = AmbulanceR
+         , BikeAgeGroup = BikeAge_Gr
+         , BikeAge = Bike_Age
+         , BikeAlcohol = Bike_Alc_D
+         , BikeDirection = Bike_Dir
+         , BikeInjury = Bike_Injur
+         , BikePosition = Bike_Pos
+         , BikeRace = Bike_Race
+         , BikeGender = Bike_Sex
+         , CrashMonth = Crash_Mont
          , CrashTime = Crash_Time
          , CrashDate = Crash_Date
          , CrashHour = Crash_Hour
-         , DriverAlcohol = Drvr_Alc_D
-         , BikeAlcohol = Bike_Alc_D) %>% 
-  mutate(CrashDate = lubridate::parse_date_time(CrashDate, "mdy I:M!:S! p"))
+         , DriverAlcohol = Drvr_Alc_D) %>% 
+  mutate(CrashDate = lubridate::parse_date_time(CrashDate, "mdy I:M!:S! p")
+         , CrashDate = as.Date(CrashDate)
+         , CrashTime = lubridate::parse_date_time(CrashTime, "%H:%M"))
+
+lubridate::year(cleanData$CrashTime) <- 2007
 
 cleanData %>% write.csv("CleanData.csv", row.names = FALSE)
