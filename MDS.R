356ace3f7dacf00505b6f080307f0bd67a08d228
Brian Fannin <BFannin@RedwoodsGroup.com>
2016-02-23 10:20:16 -0500

    Multidimensional scaling

---
+++ MDS.R
@@ -0,0 +1,20 @@
+library(dplyr)
+dfData <- readr::read_csv("CleanData.csv")
+
+dfMDS <- dfData %>% 
+  mutate(Fatality = ifelse(BikeInjury == "K: Killed", 1, -1)
+         , NoInjury = ifelse(BikeInjury == "O: No Injury", 1, -1)
+         , BikeAlcohol = ifelse(BikeAlcohol == "Yes", 1, -1)
+         , FacingTraffic = ifelse(BikeDirection == "FacingTraffic", 1, -1)
+         , Intersection = ifelse(Crash_Loc == "Intersection", 1, -1)
+         , Residential = ifelse(Developmen == "Residential", 1, -1)) %>% 
+  select(Fatality, NoInjury, BikeAlcohol, FacingTraffic, Intersection, Residential)
+
+matMDS <- t(as.matrix(dfMDS))
+
+matCovar <- matMDS %*% t(matMDS)
+matDist <- dist(matCovar)
+
+mdsAccident <- cmdscale(matDist)
+plot(mdsAccident, type = 'n')
+text(mdsAccident, rownames(matMDS))
